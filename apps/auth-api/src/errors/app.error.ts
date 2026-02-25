366a485abd2f56bbbce752d595062d19bf2bd351
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-02-25 11:07:42 +0530

    feat(auth-api): add base AppError class

--- apps/auth-api/src/errors/app.error.ts
+++ apps/auth-api/src/errors/app.error.ts
@@ -1,12 +1,26 @@
+import { ErrorCode } from "@packages/shared-types";
+
 /**
  * Base application error
- * All custom errors will extend this
+ * All custom errors MUST extend this
  */
 export class AppError extends Error {
+  public readonly statusCode: number;
+  public readonly code: ErrorCode;
+  public readonly isOperational: boolean;
+
   constructor(
-    public message: string,
-    public statusCode: number,
+    message: string,
+    code: ErrorCode,
+    statusCode = 500,
+    isOperational = true,
   ) {
     super(message);
+
+    this.code = code;
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+
+    Error.captureStackTrace(this, this.constructor);
   }
 }
